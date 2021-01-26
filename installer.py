babf71284b0f1ec681a641b030e7a391ed8de9cd
oba <neverknowsbest315@gmail.com>
2021-01-26 22:47:47 +0900

    ブランチを作成

--- installer.py
+++ installer.py
@@ -2,5 +2,5 @@ print (u"install my module")
 import sys
 sys.path.append("C:/Work/Sketch/sketch_deformer_m")
 
-import script
-reload (script)    
+import sketch_deformer_script
+reload (sketch_deformer_script)    
